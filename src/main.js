a1f591deb1b28757ea992c6d763b104a304e7c9a
Lisakarolina <w.hello@web.de>
2026-03-11 16:08:12 +0100

    Instantiate application the Vue 3 way

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
 import VeeValidate from 'vee-validate'
@@ -7,47 +7,41 @@ import DataTable from 'v-data-table'
 import { store } from './store/store'
 import json2csv from 'json2csv'
 
+import 'vuetify/styles' // Import Vuetify styles
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import App from './App.vue'
 import Admin from '@/components/Admin'
 import Form from '@/components/AttendeeForm'
 
-Vue.use(Vuetify, {
-  breakpoints: {
-    xs: 100
-  }
-});
-Vue.config.productionTip = false
-
-Vue.use(VeeValidate);
-// Vue.use(VeeValidate)
-Vue.use(VueRouter);
-Vue.use(DataTable);
-Vue.use(json2csv)
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
 const routes = [
-      {
-          path: '/admin',
-          name: 'Admin',
-          component: Admin
-      },
-      {
-          path: '/',
-          name: 'Form',
-          component: Form
-      }
-  ];
+  {
+      path: '/admin',
+      name: 'Admin',
+      component: Admin
+  },
+  {
+      path: '/',
+      name: 'Form',
+      component: Form
+  }
+];
 
-const router = new VueRouter({
-  routes: routes,
-  mode: 'history'
-});
+export const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
 
-new Vue({
-  el: '#app',
-  store: store,
-  // beforeCreate() {
-  //   this.$store.commit('initialiseStore')
-  // },
-  router: router,
-  render: h => h(App),
-});
+createApp(App)
+      .use(router)
+      .use(store)
+      .use(vuetify)
+      .mount('#app')
+
