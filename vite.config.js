2a2b2b20b79fe97210da80ae323021a4cd0196b9
Lisakarolina <w.hello@web.de>
2026-03-21 20:31:40 +0100

    Use Autoprefixer

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 import { fileURLToPath, URL } from 'node:url'
 import path from 'path'
+import autoprefixer from 'autoprefixer'
 
 const filename = fileURLToPath(import.meta.url);
 const pathSegments = path.dirname(filename);
@@ -10,6 +11,13 @@ const pathSegments = path.dirname(filename);
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vuetify({ autoImport: true }),],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer
+      ],
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(pathSegments, './src'),
