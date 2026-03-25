17771958e84c60f2b9a5dba9e3fabd8156f3a4ef
Lisakarolina <w.hello@web.de>
2026-03-25 20:17:56 +0100

    Create students overview with CRUD options

--- src/main.js
+++ src/main.js
@@ -1,50 +1,52 @@
-import { createApp } from 'vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createApp } from "vue";
+import { createRouter, createWebHashHistory } from "vue-router";
 //import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
-import * as VeeValidate from 'vee-validate';
-import { DataTable } from 'v-data-table'
-import { store } from './store/store'
-import { Parser } from '@json2csv/plainjs'
+import "vuetify/dist/vuetify.min.css"; // Ensure you are using css-loader
+import * as VeeValidate from "vee-validate";
+import { DataTable } from "v-data-table";
+import { store } from "./store/store";
+import { Parser } from "@json2csv/plainjs";
 
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import { md } from 'vuetify/iconsets/md'
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import { md } from "vuetify/iconsets/md";
 
-import App from './App.vue'
-import Admin from '@/components/Admin.vue'
-import Form from '@/components/AttendeeForm.vue'
+import App from "./App.vue";
+import Admin from "@/components/Admin.vue";
+import Form from "@/components/AttendeeForm.vue";
+import Overview from "@/components/AttendeeOverview.vue";
 
 const vuetify = createVuetify({
   icons: {
-    defaultSet: 'md',
+    defaultSet: "md",
     sets: {
       md,
     },
   },
-})
+});
 
 const routes = [
   {
-      path: '/admin',
-      name: 'Admin',
-      component: Admin
+    path: "/admin",
+    name: "Admin",
+    component: Admin,
+  },
+
+  {
+    path: "/",
+    name: "Overview",
+    component: Overview,
   },
   {
-      path: '/',
-      name: 'Form',
-      component: Form
-  }
+    path: "/add-student",
+    name: "Form",
+    component: Form,
+  },
 ];
 
 export const router = createRouter({
   history: createWebHashHistory(),
   routes,
-})
+});
 
-createApp(App)
-      .use(router)
-      .use(store)
-      .use(vuetify)
-      .mount('#app')
-
+createApp(App).use(router).use(store).use(vuetify).mount("#app");
