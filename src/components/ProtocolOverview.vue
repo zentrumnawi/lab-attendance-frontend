00955c6bef012ed5cd99852c8cdbc0479f17cd5f
Lisakarolina <w.hello@web.de>
2026-06-14 18:54:05 +0200

    Select lab day

--- src/components/ProtocolOverview.vue
+++ src/components/ProtocolOverview.vue
@@ -1,268 +1,284 @@
 <template>
-  <v-sheet border rounded>
-    <v-data-table
-      :headers="headers"
-      :items="rows"
-      expand-strategy="single"
-      item-value="id"
-      :hide-default-footer="rows.length < 11"
-      show-expand
-    >
-      <template #[`item.status`]="{ value }">
-        <v-tooltip :text="value">
-          <template #activator="{ props: tooltipProps }">
-            <v-icon
-              v-bind="tooltipProps"
-              :icon="statusIcon(value)"
-              :color="statusColor(value)"
-              size="small"
-            />
-          </template>
-        </v-tooltip>
-      </template>
+  <div>
+    <v-select
+      v-model="labDay"
+      :items="labDayOptions"
+      item-title="title"
+      item-value="value"
+      label="Versuchstag"
+      class="mb-4"
+      max-width="280"
+    />
 
-      <template #expanded-row="{ columns, item }">
-        <tr>
-          <td :colspan="columns.length">
-            <div
-              class="d-flex justify-end"
-              v-if="item.status === 'Nicht eingereicht'"
-            >
-              <v-btn
-                class="text-none"
-                color="blue-darken-4"
-                rounded="0"
-                variant="outlined"
-                text="Abgabe"
-                @click="openSubmissionDialog(item)"
+    <v-sheet border rounded>
+      <v-data-table
+        :headers="headers"
+        :items="rows"
+        expand-strategy="single"
+        item-value="id"
+        :hide-default-footer="rows.length < 11"
+        show-expand
+      >
+        <template #[`item.status`]="{ value }">
+          <v-tooltip :text="value">
+            <template #activator="{ props: tooltipProps }">
+              <v-icon
+                v-bind="tooltipProps"
+                :icon="statusIcon(value)"
+                :color="statusColor(value)"
+                size="small"
               />
-            </div>
-            <div
-              class="d-flex justify-end"
-              v-if="item.status === 'Eingereicht'"
-            >
-              <v-btn
-                class="text-none"
-                color="blue-darken-4"
-                rounded="0"
-                variant="outlined"
-                text="Abgabe akzeptieren"
-                @click="openEditedSubmissionDialog(item)"
-              />
-            </div>
-            <div
-              class="d-flex justify-space-between"
-              v-if="item.status === 'Akzeptiert'"
-            >
-              <span
-                >Akzeptiert am
-                {{ formatSubmissionDate(item.accepted_date) }}</span
+            </template>
+          </v-tooltip>
+        </template>
+
+        <template #expanded-row="{ columns, item }">
+          <tr>
+            <td :colspan="columns.length">
+              <div
+                class="d-flex justify-end"
+                v-if="item.status === 'Nicht eingereicht'"
               >
-              <v-btn
-                class="text-none"
-                color="blue-darken-4"
-                rounded="0"
-                variant="outlined"
-                text="Zurückziehen"
-                @click="openWithdrawSubmissionDialog(item)"
-              />
-            </div>
-          </td>
-        </tr>
-      </template>
-    </v-data-table>
-  </v-sheet>
+                <v-btn
+                  class="text-none"
+                  color="blue-darken-4"
+                  rounded="0"
+                  variant="outlined"
+                  text="Abgabe"
+                  @click="openSubmissionDialog(item)"
+                />
+              </div>
+              <div
+                class="d-flex justify-end"
+                v-if="item.status === 'Eingereicht'"
+              >
+                <v-btn
+                  class="text-none"
+                  color="blue-darken-4"
+                  rounded="0"
+                  variant="outlined"
+                  text="Abgabe akzeptieren"
+                  @click="openEditedSubmissionDialog(item)"
+                />
+              </div>
+              <div
+                class="d-flex justify-space-between"
+                v-if="item.status === 'Akzeptiert'"
+              >
+                <span
+                  >Akzeptiert am
+                  {{ formatSubmissionDate(item.accepted_date) }}</span
+                >
+                <v-btn
+                  class="text-none"
+                  color="blue-darken-4"
+                  rounded="0"
+                  variant="outlined"
+                  text="Zurückziehen"
+                  @click="openWithdrawSubmissionDialog(item)"
+                />
+              </div>
+            </td>
+          </tr>
+        </template>
+      </v-data-table>
+    </v-sheet>
 
-  <v-dialog v-model="submissionDialog" max-width="560">
-    <v-card title="Protokollabgabe">
-      <template #text>
-        <p v-if="selectedStudent" class="text-body-1 mb-4">
-          {{ selectedStudent.firstName }} {{ selectedStudent.name }}
-        </p>
+    <v-dialog v-model="submissionDialog" max-width="560">
+      <v-card title="Protokollabgabe">
+        <template #text>
+          <p v-if="selectedStudent" class="text-body-1 mb-4">
+            {{ selectedStudent.firstName }} {{ selectedStudent.name }}
+          </p>
 
-        <v-checkbox
-          v-model="acceptImmediately"
-          hide-details
-          label="Sofort akzeptieren"
-          @update:model-value="onAcceptChange"
-        />
+          <v-checkbox
+            v-model="acceptImmediately"
+            hide-details
+            label="Sofort akzeptieren"
+            @update:model-value="onAcceptChange"
+          />
 
-        <v-checkbox
-          v-model="requireCorrections"
-          hide-details
-          label="Korrekturen erforderlich"
-          @update:model-value="onCorrectionsChange"
-        />
+          <v-checkbox
+            v-model="requireCorrections"
+            hide-details
+            label="Korrekturen erforderlich"
+            @update:model-value="onCorrectionsChange"
+          />
 
-        <v-textarea
-          v-if="requireCorrections"
-          v-model="correctionsText"
-          auto-grow
-          class="mt-2"
-          hide-details
-          label="Erforderliche Korrekturen"
-          rows="3"
-        />
-      </template>
+          <v-textarea
+            v-if="requireCorrections"
+            v-model="correctionsText"
+            auto-grow
+            class="mt-2"
+            hide-details
+            label="Erforderliche Korrekturen"
+            rows="3"
+          />
+        </template>
 
-      <v-divider />
+        <v-divider />
 
-      <v-card-actions class="bg-surface-light">
-        <v-btn
-          text="Abbrechen"
-          variant="plain"
-          :disabled="savingSubmission"
-          @click="submissionDialog = false"
-        />
+        <v-card-actions class="bg-surface-light">
+          <v-btn
+            text="Abbrechen"
+            variant="plain"
+            :disabled="savingSubmission"
+            @click="submissionDialog = false"
+          />
 
-        <v-spacer />
+          <v-spacer />
 
-        <v-btn
-          text="Speichern"
-          :disabled="!canSaveSubmission"
-          :loading="savingSubmission"
-          @click="saveSubmission"
-        />
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+          <v-btn
+            text="Speichern"
+            :disabled="!canSaveSubmission"
+            :loading="savingSubmission"
+            @click="saveSubmission"
+          />
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
-  <v-dialog v-model="editSubmissionDialog" max-width="560">
-    <v-card title="Abgabe bearbeiten">
-      <template #text>
-        <p v-if="selectedEditStudent" class="text-body-1 mb-4">
-          {{ selectedEditStudent.firstName }} {{ selectedEditStudent.name }}
-        </p>
+    <v-dialog v-model="editSubmissionDialog" max-width="560">
+      <v-card title="Abgabe bearbeiten">
+        <template #text>
+          <p v-if="selectedEditStudent" class="text-body-1 mb-4">
+            {{ selectedEditStudent.firstName }} {{ selectedEditStudent.name }}
+          </p>
 
-        <v-list density="compact" class="bg-surface-light rounded mb-4">
-          <v-list-item>
-            <v-list-item-title>Einreichungsdatum</v-list-item-title>
-            <v-list-item-subtitle>
-              {{ formatSubmissionDate(selectedEditStudent?.submission_date) }}
-            </v-list-item-subtitle>
-          </v-list-item>
+          <v-list density="compact" class="bg-surface-light rounded mb-4">
+            <v-list-item>
+              <v-list-item-title>Einreichungsdatum</v-list-item-title>
+              <v-list-item-subtitle>
+                {{ formatSubmissionDate(selectedEditStudent?.submission_date) }}
+              </v-list-item-subtitle>
+            </v-list-item>
 
-          <v-list-item>
-            <v-list-item-title>Erforderliche Korrekturen</v-list-item-title>
-            <v-list-item-subtitle>
-              <v-textarea
-                :model-value="selectedEditStudent?.necessary_corrections ?? ''"
-                auto-grow
-                class="mt-2"
-                hide-details
-                label="Erforderliche Korrekturen"
-                rows="3"
-              />
-            </v-list-item-subtitle>
-          </v-list-item>
-        </v-list>
+            <v-list-item>
+              <v-list-item-title>Erforderliche Korrekturen</v-list-item-title>
+              <v-list-item-subtitle>
+                <v-textarea
+                  :model-value="
+                    selectedEditStudent?.necessary_corrections ?? ''
+                  "
+                  auto-grow
+                  class="mt-2"
+                  hide-details
+                  label="Erforderliche Korrekturen"
+                  rows="3"
+                />
+              </v-list-item-subtitle>
+            </v-list-item>
+          </v-list>
 
-        <v-checkbox
-          v-model="acceptSubmission"
-          hide-details
-          label="Abgabe akzeptieren"
-        />
-      </template>
+          <v-checkbox
+            v-model="acceptSubmission"
+            hide-details
+            label="Abgabe akzeptieren"
+          />
+        </template>
 
-      <v-divider />
+        <v-divider />
 
-      <v-card-actions class="bg-surface-light">
-        <v-btn
-          text="Abbrechen"
-          variant="plain"
-          :disabled="savingEditSubmission"
-          @click="editSubmissionDialog = false"
-        />
+        <v-card-actions class="bg-surface-light">
+          <v-btn
+            text="Abbrechen"
+            variant="plain"
+            :disabled="savingEditSubmission"
+            @click="editSubmissionDialog = false"
+          />
 
-        <v-spacer />
+          <v-spacer />
 
-        <v-btn
-          text="Speichern"
-          :disabled="!acceptSubmission"
-          :loading="savingEditSubmission"
-          @click="saveEditedSubmission"
-        />
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+          <v-btn
+            text="Speichern"
+            :disabled="!acceptSubmission"
+            :loading="savingEditSubmission"
+            @click="saveEditedSubmission"
+          />
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
-  <v-dialog v-model="withdrawSubmissionDialog" max-width="560">
-    <v-card title="Abgabe zurückziehen">
-      <template #text>
-        <p v-if="selectedEditStudent" class="text-body-1 mb-4">
-          {{ selectedEditStudent.firstName }} {{ selectedEditStudent.name }}
-        </p>
+    <v-dialog v-model="withdrawSubmissionDialog" max-width="560">
+      <v-card title="Abgabe zurückziehen">
+        <template #text>
+          <p v-if="selectedEditStudent" class="text-body-1 mb-4">
+            {{ selectedEditStudent.firstName }} {{ selectedEditStudent.name }}
+          </p>
 
-        <v-list density="compact" class="bg-surface-light rounded mb-4">
-          <v-list-item>
-            <v-list-item-title>Einreichungsdatum</v-list-item-title>
-            <v-list-item-subtitle>
-              {{ formatSubmissionDate(selectedEditStudent?.submission_date) }}
-            </v-list-item-subtitle>
-          </v-list-item>
+          <v-list density="compact" class="bg-surface-light rounded mb-4">
+            <v-list-item>
+              <v-list-item-title>Einreichungsdatum</v-list-item-title>
+              <v-list-item-subtitle>
+                {{ formatSubmissionDate(selectedEditStudent?.submission_date) }}
+              </v-list-item-subtitle>
+            </v-list-item>
 
-          <v-list-item>
-            <v-list-item-title>Erforderliche Korrekturen</v-list-item-title>
-            <v-list-item-subtitle>
-              <v-textarea
-                :model-value="selectedEditStudent?.necessary_corrections ?? ''"
-                auto-grow
-                class="mt-2"
-                hide-details
-                label="Erforderliche Korrekturen"
-                rows="3"
-              />
-            </v-list-item-subtitle>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-title>Akzeptiert am</v-list-item-title>
-            <v-list-item-subtitle>
-              {{
-                formatSubmissionDate(
-                  selectedEditStudent?.accepted_date ?? null,
-                ) ?? "—"
-              }}
-            </v-list-item-subtitle>
-          </v-list-item>
-        </v-list>
+            <v-list-item>
+              <v-list-item-title>Erforderliche Korrekturen</v-list-item-title>
+              <v-list-item-subtitle>
+                <v-textarea
+                  :model-value="
+                    selectedEditStudent?.necessary_corrections ?? ''
+                  "
+                  auto-grow
+                  class="mt-2"
+                  hide-details
+                  label="Erforderliche Korrekturen"
+                  rows="3"
+                />
+              </v-list-item-subtitle>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title>Akzeptiert am</v-list-item-title>
+              <v-list-item-subtitle>
+                {{
+                  formatSubmissionDate(
+                    selectedEditStudent?.accepted_date ?? null,
+                  ) ?? "—"
+                }}
+              </v-list-item-subtitle>
+            </v-list-item>
+          </v-list>
 
-        <v-checkbox
-          v-model="withdrawSubmission"
-          hide-details
-          label="Abgabe zurückziehen"
-        />
-        <span class="text-body-2"
-          >Diese Aktion wird den Status der Abgabe auf "eingereicht"
-          zurücksetzen.</span
-        >
-      </template>
+          <v-checkbox
+            v-model="withdrawSubmission"
+            hide-details
+            label="Abgabe zurückziehen"
+          />
+          <span class="text-body-2"
+            >Diese Aktion wird den Status der Abgabe auf "eingereicht"
+            zurücksetzen.</span
+          >
+        </template>
 
-      <v-divider />
+        <v-divider />
 
-      <v-card-actions class="bg-surface-light">
-        <v-btn
-          text="Abbrechen"
-          variant="plain"
-          :disabled="savingEditSubmission"
-          @click="withdrawSubmissionDialog = false"
-        />
+        <v-card-actions class="bg-surface-light">
+          <v-btn
+            text="Abbrechen"
+            variant="plain"
+            :disabled="savingEditSubmission"
+            @click="withdrawSubmissionDialog = false"
+          />
 
-        <v-spacer />
+          <v-spacer />
 
-        <v-btn
-          text="Speichern"
-          :disabled="!withdrawSubmission"
-          :loading="savingEditSubmission"
-          @click="saveWithdrawSubmission"
-        />
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+          <v-btn
+            text="Speichern"
+            :disabled="!withdrawSubmission"
+            :loading="savingEditSubmission"
+            @click="saveWithdrawSubmission"
+          />
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { useProtocolStore } from "@/stores/protocols";
-import { computed, onMounted, ref, shallowRef } from "vue";
+import { computed, onMounted, ref, shallowRef, watch } from "vue";
 import { useAttendeeStore } from "@/stores/attendeeStore";
 import { format } from "date-fns";
 
@@ -289,14 +305,14 @@ const headers: {
   width: string;
 }[] = [
   { title: "Name", key: "name", align: "start", width: "40%" },
-  { title: "First Name", key: "firstName", align: "start", width: "40%" },
+  { title: "Vorname", key: "firstName", align: "start", width: "40%" },
   {
-    title: "Matriculation Number",
+    title: "Matrikelnummer",
     key: "matriculationNumber",
     align: "start",
     width: "20%",
   },
-  { title: "Lab Partner", key: "labPartner", align: "start", width: "20%" },
+  { title: "Labor-Partner", key: "labPartner", align: "start", width: "20%" },
   { title: "Status", key: "status", align: "center", width: "20%" },
 ];
 
@@ -325,6 +341,10 @@ function statusColor(status: ProtocolStatus): string {
 const store = useProtocolStore();
 const attendeeStore = useAttendeeStore();
 const labDay = ref(1);
+const labDayOptions = Array.from({ length: 8 }, (_, index) => ({
+  title: `Versuchstag ${index + 1}`,
+  value: index + 1,
+}));
 const submissionDialog = shallowRef(false);
 const editSubmissionDialog = shallowRef(false);
 const selectedStudent = ref<ProtocolRow | null>(null);
@@ -468,6 +488,10 @@ onMounted(async () => {
     attendeeStore.fetchStudents(),
     store.fetchProtocols(labDay.value),
   ]);
+});
+
+watch(labDay, (day) => {
+  void store.fetchProtocols(day);
 });
 
 const rows = computed<ProtocolRow[]>(() =>
