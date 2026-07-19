8827460e147d58e8a658ac18e5fc521cac4cf64f
Lisakarolina <w.hello@web.de>
2026-07-19 16:15:31 +0200

    Only admins edit exercises

--- src/components/ExercisesOverview.vue
+++ src/components/ExercisesOverview.vue
@@ -123,9 +123,23 @@ const formModel = ref(createNewRecord());
 const dialog = shallowRef(false);
 const isEditing = computed(() => !!formModel.value.id);
 const isSuperuser = computed(() => AuthStore.isSuperuser);
-const headers = [
-  { title: "Titel", key: "title", align: "start" as const },
-  { title: "Aktion", key: "actions", align: "end" as const, sortable: false },
+const headers: {
+  title: string;
+  key: string;
+  align: "start" | "end";
+  sortable: boolean;
+}[] = [
+  { title: "Titel", key: "title", align: "start" as const, sortable: true },
+  ...(isSuperuser.value
+    ? [
+        {
+          title: "Aktion",
+          key: "actions",
+          align: "end" as const,
+          sortable: false,
+        },
+      ]
+    : []),
 ];
 
 function add() {
